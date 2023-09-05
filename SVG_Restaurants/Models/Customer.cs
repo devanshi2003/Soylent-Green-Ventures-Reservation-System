976ca702580d7340d73b3f649b7a0f0efe65cf42
mainguyen <nnmai0606@gmail.com>
2023-09-05 15:03:31 +0930

    feat: make all the form elements required to fill in

--- SVG_Restaurants/Models/Customer.cs
+++ SVG_Restaurants/Models/Customer.cs
@@ -14,21 +14,27 @@ namespace SVG_Restaurants.Models
         public int CustomerId { get; set; }
 
         [Display(Name = "First Name")]
+        [Required(ErrorMessage = "{0} is required.")]
         public string? FirstName { get; set; }
 
         [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "{0} is required.")]
         public string? LastName { get; set; }
 
         [Display(Name = "Email")]
+        [Required(ErrorMessage = "{0} is required.")]
         public string? Email { get; set; }
 
         [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = "{0} is required.")]
         public string? PhoneNumber { get; set; }
 
         [Display(Name = "Username")]
+        [Required(ErrorMessage = "{0} is required.")]
         public string? Username { get; set; }
 
         [Display(Name = "Password")]
+        [Required(ErrorMessage = "{0} is required.")]
         public string? Password { get; set; }
         public int? LoyaltyPoints { get; set; }
 
