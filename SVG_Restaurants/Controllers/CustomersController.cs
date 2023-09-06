1d2ef7fc0f55c431034da01a5d1f54ac46ea0039
Devanshi <devanshiapatel2003@gmail.com>
2023-09-06 18:55:16 +0930

    Add queries to delete customer account

--- SVG_Restaurants/Controllers/CustomersController.cs
+++ SVG_Restaurants/Controllers/CustomersController.cs
@@ -91,7 +91,7 @@ namespace SVG_Restaurants.Controllers
             {
                 _context.Add(customer);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Login", "Customers");
             }
             return View(customer);
         }
@@ -168,20 +168,27 @@ namespace SVG_Restaurants.Controllers
         // POST: Customers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int CustomerID)
         {
-            if (_context.Customers == null)
+            try
             {
-                return Problem("Entity set 'SGVContext.Customers'  is null.");
-            }
-            var customer = await _context.Customers.FindAsync(id);
-            if (customer != null)
-            {
+                // Attempt to find the customer with the specified CustomerId
+                var customer = _context.Customers.Single(c => c.CustomerId == CustomerID);
+
+                // Remove the customer entity
                 _context.Customers.Remove(customer);
+
+                // Save the changes to the database
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
             }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            catch (InvalidOperationException) {
+                return NotFound(); // Handle the case where the customer is not found.
+            }
+
+
+
         }
 
         private bool CustomerExists(int id)
