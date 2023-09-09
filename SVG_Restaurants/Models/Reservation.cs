2d5d4aa807e3f766a674b4d0370c7118f2b20133
Devanshi <devanshiapatel2003@gmail.com>
2023-09-09 18:24:04 +0930

    Add model display names

--- SVG_Restaurants/Models/Reservation.cs
+++ SVG_Restaurants/Models/Reservation.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace SVG_Restaurants.Models
 {
@@ -8,12 +10,28 @@ namespace SVG_Restaurants.Models
         public int ReservationId { get; set; }
         public int? CustomerId { get; set; }
         public int? RestaurantId { get; set; }
+
+
+        [Display(Name = "Dining Area Preference")]
+        [Required(ErrorMessage = "{0} is required.")]
         public int? AreaId { get; set; }
+
+        [Display(Name = "Reservation Time")]
+        [Required(ErrorMessage = "{0} is required.")]
         public DateTime? ReservationTiming { get; set; }
+
+        [Display(Name = "Banquet Name")]
         public int? BanquetId { get; set; }
+
+        [Display(Name = "Number of People")]
+        [Required(ErrorMessage = "{0} is required.")]
         public int? NumberOfPeople { get; set; }
         public int? GuestId { get; set; }
+
+        [Display(Name = "Baby Seats")]
         public int? HighChairs { get; set; }
+
+        [Display(Name = "Special Notes")]
         public string? SpecialNotes { get; set; }
 
         public virtual DiningArea? Area { get; set; }
