26ba8da180ffdcd25c45e7cdabc6f029e9969677
Devanshi <devanshiapatel2003@gmail.com>
2023-09-10 19:31:14 +0930

    Create query to find available seats

--- SVG_Restaurants/Controllers/ReservationsController.cs
+++ SVG_Restaurants/Controllers/ReservationsController.cs
@@ -192,9 +192,54 @@ namespace SVG_Restaurants.Controllers
         }
 
 
+        public int GetAvailableSeats(DateTime selectedDateTime, int RestaurantId)
+        {
+            var timeFrames = new List<(int startHour, int endHour)>
+                {
+                    (12, 14),
+                    (17, 19),
+                    (19, 21),
+                    (10, 12),
+                    (12, 14),
+                    (21, 23),
+                };
 
-        // GET: Reservations/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+            foreach (var timeFrame in timeFrames)
+            {
+                int startHour = timeFrame.startHour;
+                int endHour = timeFrame.endHour;
+
+                if (selectedDateTime.Hour >= startHour && selectedDateTime.Hour < endHour)
+                {
+
+                    var sumOfNumberOfPeople = _context.Reservations
+                       .Where(r => r.RestaurantId == RestaurantId &&
+                                r.ReservationTiming.HasValue &&
+                                r.ReservationTiming.Value.Date == selectedDateTime.Date &&
+                                r.ReservationTiming.Value.Hour >= startHour &&
+                                r.ReservationTiming.Value.Hour <= endHour)
+                       .Sum(r => r.NumberOfPeople);
+
+
+                    Debug.WriteLine(sumOfNumberOfPeople);
+
+                    var seatCapacity = _context.Restaurants
+                        .Where(c => c.RestaurantId == RestaurantId)
+                        .Select(c => c.SeatCapacity)
+                        .FirstOrDefault();
+
+                    int availableSeats = (int)seatCapacity - (int)sumOfNumberOfPeople;
+                    return availableSeats;
+
+                }
+
+            }
+            return -1;
+        }
+
+
+            // GET: Reservations/Edit/5
+            public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Reservations == null)
             {
