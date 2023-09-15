6d0562a7687bc91a422b3c977b4df120b65d780d
Yunmei Ye <yeyyy013@mymail.unisa.edu.au>
2023-09-15 17:31:16 +0930

    Added a non-mapped property FullName for Guest

--- SVG_Restaurants/Models/Guest.cs
+++ SVG_Restaurants/Models/Guest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
 
 namespace SVG_Restaurants.Models
 {
@@ -15,6 +18,16 @@ namespace SVG_Restaurants.Models
         public string? LastName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
+
+        [Display(Name = "Customer Name")]
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
 
         public virtual ICollection<Reservation> Reservations { get; set; }
     }
